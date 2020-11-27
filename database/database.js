5e9fa3673e19f3cd0b40a3ee63409ba4fed9294a
André Nishimoto <42229054+LostScream@users.noreply.github.com>
2020-11-27 01:46:46 -0300

    Update database.js
    Correção no arquivo de conexão com o BD.

--- database/database.js
+++ database/database.js
@@ -1,7 +1,6 @@
-const { Sequelize } = require("sequelize");
 const sequelize = require("sequelize");
 
-const connection = new Sequelize("Projeto1","teste","12345",{
+const connection = new sequelize("Projeto1","teste","12345",{
     host: "localhost",
     dialect: "mysql"
 });
